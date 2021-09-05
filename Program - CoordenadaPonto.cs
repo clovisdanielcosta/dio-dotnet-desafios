156dc0c650a7d2c56d5d70f01ec717459b563f3b
clovisdanielcosta <clovisdanielcosta@gmail.com>
2021-09-05 18:01:28 -0300

    Commits of challenges CoordenadaPonto, Media 3, StringCabendoString e TrigoNoTabuleiro

---
+++ Program - CoordenadaPonto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace dio_dotnet_desafios
+{
+    public class Program4
+    {
+        public static void Main4(string[] args)
+        {
+        //complete os espaços em branco com sua solução para o problema
+            string[] valores = ("3.5 -2").Split(" ");
+            double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+            if (x == 0 && y == 0) Console.WriteLine("Origem");
+            else if (x == 0 && y > 0 ||x == 0 && y < 0) Console.WriteLine("Eixo Y");
+            else if (y == 0 && x > 0 ||y == 0 && x < 0) Console.WriteLine("Eixo X");
+            else if (x > 0  && y > 0) Console.WriteLine("Q1");
+            else if (x < 0 && y > 0) Console.WriteLine("Q2");
+            else if (x < 0 && y < 0) Console.WriteLine("Q3");
+            else if (x > 0 && y < 0) Console.WriteLine("Q4");
+            //else Console.WriteLine("Q4");
+        }
+    }
+}
+
---
+++ Program - TrigoNoTabuleiro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace dio_dotnet_desafios
+{
+    public class Program2
+    {
+        public static void Main2(string[] args)
+        {
+            int qtdTeste = 9;
+            int[] valoresRecebidos = {0, 8, 16, 24, 32, 40, 48, 56, 64};
+            
+            for (int i = 0; i < qtdTeste; i++)
+            {
+                var graos = new BigInteger(1);
+                int casas = valoresRecebidos[i];
+             
+                while (casas != 0){
+                    graos = graos * 2;
+                    casas--;
+                }
+            Console.WriteLine($"{graos/12000} kg");
+            }
+
+        }
+    }
+}
