06facc4393d058e9f9ee9f69f07c935d14793cc2
clovisdanielcosta <clovisdanielcosta@gmail.com>
2021-09-05 22:57:35 -0300

    Created challenge Tempo de Jogo com Minutos

--- Program - Tempo de Jogo com Minutos.cs
+++ Program - Tempo de Jogo com Minutos.cs
@@ -2,9 +2,9 @@
 
 namespace dio_dotnet_desafios
 {
-    public class Program
+    public class ProgramCOPY
     {
-        public static void Main(string[] args)
+        public static void MainCOPY(string[] args)
         {
             int hhInicial, hhFinal, mmInicial, mmFinal;
             string[] valoresRecebidos = {
@@ -13,10 +13,10 @@ namespace dio_dotnet_desafios
                     "7 10 9 10",    //O JOGO DUROU 2 HORA(S) E 0 MINUTO(S)
                     "9 8 7 10",     //O JOGO DUROU 22 HORA(S) E 2 MINUTO(S)
                     "9 10 7 8",     //O JOGO DUROU 21 HORA(S) E 58 MINUTO(S)
-                    "10 10 10 10", //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
-                    "9 10 10 10",  //O JOGO DUROU 1 HORA(S) E 0 MINUTO(S)
-                    "10 10 9 10",  //O JOGO DUROU 23 HORA(S) E 0 MINUTO(S)
-                    "7 7 7 7",     //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
+                     "10 10 10 10", //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
+                     "9 10 10 10",  //O JOGO DUROU 1 HORA(S) E 0 MINUTO(S)
+                     "10 10 9 10",  //O JOGO DUROU 23 HORA(S) E 0 MINUTO(S)
+                     "7 7 7 7",     //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
                     "7 8 9 10",     //O JOGO DUROU 2 HORA(S) E 2 MINUTO(S)
                     "7 10 8 9",     //O JOGO DUROU 0 HORA(S) E 59 MINUTO(S)
                     "7 9 6 1",      //O JOGO DUROU 22 HORA(S) E 52 MINUTO(S)
@@ -24,22 +24,12 @@ namespace dio_dotnet_desafios
                     "9 10 9 10",    //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
                     "5 15 2 10",    //O JOGO DUROU 20 HORA(S) E 55 MINUTO(S)
                     "0 2 24 5",     //O JOGO DUROU 24 HORA(S) E 3 MINUTO(S)
-                    "24 60 24 59",  //O JOGO DUROU 23 HORA(S) E 59 MINUTO(S)
-                    "0 0 0 0",      //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
-                    "0 0 0 1",      //O JOGO DUROU 0 HORA(S) E 1 MINUTO(S)
-                    "0 0 1 0",      //O JOGO DUROU 1 HORA(S) E 0 MINUTO(S)
-                    "0 1 0 0",      //O JOGO DUROU 23 HORA(S) E 59 MINUTO(S)
-                    "1 0 0 0",      //O JOGO DUROU 23 HORA(S) E 0 MINUTO(S)
-                    "7 10 10 7",   //O JOGO DUROU 2 HORA(S) E 57 MINUTO(S)
-                    "7 10 10 10",  //O JOGO DUROU 3 HORA(S) E 0 MINUTO(S) 
-                    "7 10 10 7",   //O JOGO DUROU 2 HORA(S) E 57 MINUTO(S)
-                    "7 10 7 7",    //O JOGO DUROU 23 HORA(S) E 57 MINUTO(S)
-                    "7 7 7 7",     //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
-                    "7 7 7 10",    //O JOGO DUROU 0 HORA(S) E 3 MINUTO(S)
-                    "7 7 10 10",   //O JOGO DUROU 3 HORA(S) E 3 MINUTO(S) 
-                    "10 7 7 10",   //O JOGO DUROU 21 HORA(S) E 3 MINUTO(S)
-                    "10 10 7 7",   //O JOGO DUROU 20 HORA(S) E 57 MINUTO(S)
-                    "10 10 7 10",  //O JOGO DUROU 21 HORA(S) E 0 MINUTO(S)  
+                    "24 60 24 59",   //O JOGO DUROU 23 HORA(S) E 59 MINUTO(S)
+                    "0 0 0 0",   //O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
+                    "0 0 0 1",   //O JOGO DUROU 0 HORA(S) E 1 MINUTO(S)
+                    "0 0 1 0",   //O JOGO DUROU 1 HORA(S) E 0 MINUTO(S)
+                    "0 1 0 0",   //O JOGO DUROU 23 HORA(S) E 59 MINUTO(S)
+                    "1 0 0 0",   //O JOGO DUROU 23 HORA(S) E 0 MINUTO(S)
                 };
            
            for (int i = 0; i < valoresRecebidos.Length; i++)
